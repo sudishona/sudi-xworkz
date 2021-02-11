f4cf4467923401387498e98b9530aef5a9a483e8
sudishona <shivanand206@gmail.com>
2021-02-11 11:33:02 +0530

    This is data types program

---
+++ Laptop.java
@@ -0,0 +1,16 @@
+public class Laptop
+{
+static String name="Dell";
+public static void main(String a[])
+{
+String name1="lenovo";
+String name2="Hp";
+Intel();
+System.out.println("name1");
+System.out.println("name2");
+}
+public static void Intel()
+{
+System.out.println(name);
+}
+}
