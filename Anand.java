7891c34467a1156a2a7108368c520f24c3e6eaed
sudishona <shivanand206@gmail.com>
2021-02-19 10:22:19 +0530

    simple program

---
+++ Anand.java
@@ -0,0 +1,12 @@
+public class Anand
+{
+String broName;
+int broBrandid;
+public Anand(String Name, int Brandid)
+{
+broName=Name;
+broBrandid=Brandid;
+System.out.println("Family Time");
+System.out.println("Family fun");
+}
+     }
