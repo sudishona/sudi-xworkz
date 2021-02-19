c21ab4504f9fc87d715fbaf55148909a3eb6d633
sudishona <shivanand206@gmail.com>
2021-02-19 10:11:09 +0530

    example of instance variable

---
+++ Bank.java
@@ -0,0 +1,13 @@
+public class Bank
+{
+String bankName;
+String branchName;
+String ifscCode;
+public Bank(String name, String branch, String code)
+{
+bankName=name;
+branchName=branch;
+ifscCode=code;
+System.out.println("Bank object is created");
+}
+}
